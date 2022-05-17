6db4b18b1c257ea25f0bbccdb7fc9b1e717a8f77
pritambhalnor <70471408+pritambhalnor@users.noreply.github.com>
2022-05-17 09:48:30 +0530

    Update sendmail.php

--- PhpMail/sendmail.php
+++ PhpMail/sendmail.php
@@ -19,7 +19,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'no-reply@anzilsoft.com';                     //SMTP username
-    $mail->Password   = 'cmdalwxnbvaczxyo';                               //SMTP password
+    $mail->Password   = '1cmdalwxnbvaczxyop';                               //SMTP password
     $mail->SMTPSecure =  PHPMailer::ENCRYPTION_STARTTLS; // PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
